e55a6315f5f5bbe1b9f3dbfc8202ec6df66b913b
wyvernbw <iamwyvern511@gmail.com>
2025-06-24 01:27:21 +0300

    Initial Commit

---
+++ islet/isletApp.swift
@@ -0,0 +1,17 @@
+//
+//  isletApp.swift
+//  islet
+//
+//  Created by calin on 24.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct isletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ isletTests/isletTests.swift
@@ -0,0 +1,16 @@
+//
+//  isletTests.swift
+//  isletTests
+//
+//  Created by calin on 24.06.2025.
+//
+
+import Testing
+
+struct isletTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
