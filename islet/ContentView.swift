0cac419c670d9164ac6964e60c665635c691966d
wyvernbw <iamwyvern511@gmail.com>
2025-06-24 12:56:48 +0300

    add hover state

--- islet/ContentView.swift
+++ islet/ContentView.swift
@@ -8,30 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
-    @ObservedObject var viewModel: ViewModel 
+    @ObservedObject var viewModel: ViewModel
 
     var body: some View {
 
         ZStack {
             RoundedRectangle(cornerRadius: 8)
-                .fill(Color.white).ignoresSafeArea()
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+                .fill(Color.black).ignoresSafeArea()
+            VStack {
+            }
+
         }
         .ignoresSafeArea()
         .onHover {
             isHovered in
-            switch (self.viewModel.sizeState, isHovered) {
-            case (.idle, true):
-                self.viewModel.reduceSizeState(event: .hovered)
-            case (.hover, false):
-                self.viewModel.reduceSizeState(event: .unhovered)
-            default:
-                break
+            switch isHovered {
+            case true: self.viewModel.reduceSizeState(event: .hovered)
+            case false: self.viewModel.reduceSizeState(event: .unhovered)
             }
         }
     }
 }
-
--- islet/isletApp.swift
+++ islet/isletApp.swift
@@ -10,7 +10,7 @@ import SwiftUI
 @main
 struct isletApp: App {
     @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
-    
+
     var body: some Scene {
         Settings {
             EmptyView()
@@ -20,30 +20,37 @@ struct isletApp: App {
 
 class MainNSPanel: NSPanel {
     override var canBecomeMain: Bool {
-        return true;
+        return true
     }
     override var canBecomeKey: Bool {
-        return true;
+        return true
     }
 
 }
 
 class AppDelegate: NSObject, NSApplicationDelegate {
     var window: MainNSPanel!
-    
+
     func applicationDidFinishLaunching(_ notification: Notification) {
+
         let viewModel = ViewModel(app: self)
+
         // Create the SwiftUI view
         let contentView = ContentView(viewModel: viewModel)
 
+        let rect = {
+            var rect = NSRect()
+            rect.size = viewModel.sizeState.asSize()
+            return rect.atScreenCenter()
+        }
         // Create the borderless window
         let panel = MainNSPanel.init(
-            contentRect: NSRect(x: 0, y: 0, width: 256, height: 32),
+            contentRect: rect(),
             styleMask: [.borderless, .nonactivatingPanel],
             backing: .buffered,
             defer: false,
         )
-        
+
         panel.isOpaque = false
         panel.backgroundColor = NSColor.clear
         //panel.setFrameAutosaveName("Main Window")
@@ -56,23 +63,25 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         // panel.isFloatingPanel = true
 
         self.window = panel
-        self.positionWindowAtTopCenter()
-
     }
-    
-    func positionWindowAtTopCenter() {
-        print(self.window.frame.origin)
-        guard let screen = NSScreen.main else { return }
-        print("we got screen")
+}
+
+extension NSRect {
+    func atScreenCenter() -> NSRect {
+        guard let screen = NSScreen.main else { return self }
         let screenFrame = screen.frame
-        let windowSize = self.window.frame.size
-        
+        let windowSize = self.size
+
         let xPos = screenFrame.origin.x + (screenFrame.width - windowSize.width) / 2
-        let yPos = screenFrame.origin.y + screenFrame.height - windowSize.height
-        
-        self.window.setFrameOrigin(NSPoint(x: xPos, y: yPos));
-        
-        print(self.window.frame.origin)
+        let yPos = screenFrame.origin.y + screenFrame.height - windowSize.height + 6
+
+        return NSRect(
+            x: xPos,
+            y: yPos,
+            width: self.width,
+            height: self.height
+        )
+
     }
 }
 
@@ -81,28 +90,93 @@ enum AppSizeStateEvent {
     case unhovered
 }
 
+enum Information {
+
+}
+
 enum AppSizeState {
-    case idle
+    case hidden
+    case idle(
+        info: [Information]
+    )
     case hover
-    
+
     func reduce(event: AppSizeStateEvent) -> AppSizeState {
         switch (self, event) {
-            case (.idle, .hovered):
-                return .hover
-            case(.hover, .unhovered):
-                return .idle
-            default:
-                return self
+        case (.idle, .hovered):
+            return .hover
+        case (.hover, .unhovered):
+            let info = getInfo()
+            if info.isEmpty {
+                return .hidden
+            }
+            return .idle(info: info)
+        case (_, .unhovered):
+            fatalError("unreachable")
+        case (.hidden, .hovered):
+            return .hover
+        case (.hover, .hovered):
+            fatalError("unreachable")
         }
     }
+
+    func getInfo() -> [Information] {
+        return []
+    }
+
+    func asSize() -> NSSize {
+        switch self {
+        case .hidden: return Self.hiddenFrame
+        case .idle: return Self.idleFrame
+        case .hover: return Self.hoverFrame
+        }
+    }
+
+    static let hiddenFrame = NSSize(width: 174, height: notch + 6)
+    static let idleFrame = NSSize(width: 256, height: notch + 6)
+    static let hoverFrame = NSSize(width: 260, height: notch + 4 + 6)
+    static let notch = 32
 }
 
 class ViewModel: ObservableObject {
-    var app: AppDelegate!;
-    @Published var sizeState: AppSizeState = .idle {
+    var app: AppDelegate
+    @Published var sizeState: AppSizeState = .hidden {
         didSet {
-            print("sizeState changed from \(oldValue) to \(sizeState)")
+            switch (oldValue, sizeState) {
+            case (_, .hover):
+                self.resizeTo(size: AppSizeState.hoverFrame)
+                NSHapticFeedbackManager.defaultPerformer.perform(.generic, performanceTime: .now)
+            case (.hover, .idle):
+                self.resizeTo(size: AppSizeState.idleFrame, duration: 0.2)
+                NSHapticFeedbackManager.defaultPerformer.perform(.generic, performanceTime: .now)
+            case (.idle, .hidden):
+                self.resizeTo(size: AppSizeState.hiddenFrame, duration: 0.2)
+            case (.hidden, .idle):
+                self.resizeTo(size: AppSizeState.hiddenFrame, duration: 0.2)
+            case (.hover, .hidden):
+                self.resizeTo(size: AppSizeState.hiddenFrame, duration: 0.2)
+                NSHapticFeedbackManager.defaultPerformer.perform(.generic, performanceTime: .now)
+            case (.idle, .idle):
+                break
+            case (.hidden, .hidden):
+                break
+            }
         }
+    }
+
+    func resizeTo(
+        size: NSSize, easing: CAMediaTimingFunctionName = .easeOut, duration: TimeInterval = 0.15
+    ) {
+        let frame = {
+            var frame = self.app.window.frame
+            frame.size = size
+            return frame.atScreenCenter()
+        }
+        NSAnimationContext.runAnimationGroup({ context in
+            context.duration = duration
+            context.timingFunction = CAMediaTimingFunction(name: easing)
+            self.app.window.animator().setFrame(frame(), display: true)
+        })
     }
 
     init(app: AppDelegate) {
